174ac94aaf7c30b2e7de54996b6c7780c11bab2c
jermaincoffie <jermaincoffie@gmail.com>
2026-06-02 21:35:59 +0200

    fix: remember me links, forgot password rechts op zelfde regel

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,20 +25,17 @@
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
             </div>
 
-            <div class="block mt-4">
+            <div class="flex items-center justify-between mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
                     <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
-
-            @if (Route::has('password.request'))
-            <div class="flex justify-end mt-4">
+                @if (Route::has('password.request'))
                 <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                     {{ __('Forgot your password?') }}
                 </a>
+                @endif
             </div>
-            @endif
 
             <div class="mt-4">
                 <x-button class="w-full justify-center">
