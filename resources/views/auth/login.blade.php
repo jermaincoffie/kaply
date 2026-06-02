28dfd845f8a07b90382e38b588866386aca7f5a5
jermaincoffie <jermaincoffie@gmail.com>
2026-06-02 21:33:41 +0200

    fix: login layout - forgot password links, log in rechts

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -32,14 +32,14 @@
                 </label>
             </div>
 
-            <div class="flex items-center justify-end mt-4">
+            <div class="flex items-center justify-between mt-4">
                 @if (Route::has('password.request'))
                     <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.request') }}">
                         {{ __('Forgot your password?') }}
                     </a>
                 @endif
 
-                <x-button class="ms-4">
+                <x-button>
                     {{ __('Log in') }}
                 </x-button>
             </div>
