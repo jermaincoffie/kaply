3cc6f358182595951277b235e145bd9413ac75f6
jermaincoffie <jermaincoffie@gmail.com>
2026-06-13 16:16:41 +0200

    style: header hoogte terug h-14, logo groter op login scherm (h-20)

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,6 +1,6 @@
 ﻿<a href="/">
-    <img src="{{ asset('images/kaply-logo-light.png') }}" class="block dark:hidden h-12 w-auto" alt="Kaply">
-    <img src="{{ asset('images/kaply-logo-dark.png') }}" class="hidden dark:block h-12 w-auto" alt="Kaply">
+    <img src="{{ asset('images/kaply-logo-light.png') }}" class="block dark:hidden h-20 w-auto" alt="Kaply">
+    <img src="{{ asset('images/kaply-logo-dark.png') }}" class="hidden dark:block h-20 w-auto" alt="Kaply">
 </a>
 
 
