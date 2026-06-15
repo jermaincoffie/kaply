62b5218d280f17b2e9668205dc135d30d114b4f9
jermaincoffie <jermaincoffie@gmail.com>
2026-06-15 16:48:24 +0200

    fix: voeg Alpine.js CDN toe aan voor-kappers pagina (FAQ accordion werkt niet zonder Alpine)

--- resources/views/voor-kappers.blade.php
+++ resources/views/voor-kappers.blade.php
@@ -9,6 +9,7 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,500,600,700,800&display=swap" rel="stylesheet" />
     @vite(['resources/css/app.css', 'resources/js/app.js'])
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
 </head>
 <body class="font-sans antialiased bg-white text-gray-900">
 
