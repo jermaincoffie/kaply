32a23a33a28265fb9a8a9d0904b8348d697c1f82
jermaincoffie <jermaincoffie@gmail.com>
2026-07-06 17:52:16 +0200

    debug: log push subscribe request

--- app/Http/Controllers/PushSubscriptionController.php
+++ app/Http/Controllers/PushSubscriptionController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Log;
 
 class PushSubscriptionController extends Controller
 {
     public function store(Request $request)
     {
+        Log::info('Push subscribe request van user ' . ($request->user()?->id ?? 'geen') . ' UA: ' . substr($request->userAgent() ?? '', 0, 80));
+
         $request->validate([
             'endpoint'    => ['required', 'url'],
             'keys.auth'   => ['required', 'string'],
